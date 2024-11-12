ff0c96fee1bfaf9ba2a8bb1206831d2f683bec77
Einstean <15620242877@163.com>
2024-11-12 03:43:38 -0800

    1

---
+++ CPBM_frontend/unpackage/dist/dev/mp-weixin/uni-starter.config.js
@@ -0,0 +1,31 @@
+"use strict";
+const config = {
+  "h5": {
+    "url": "https://uni-starter.dcloud.net.cn",
+    "openApp": {}
+  },
+  "mp": {
+    "weixin": {
+      "id": ""
+    }
+  },
+  "about": {
+    "appName": "CoHub",
+    "logo": "/static/logo.png",
+    "company": "The University of Hong Kong",
+    "download": "https://itunes.apple.com/cn/app/hello-uni-app/id1417078253?mt=8",
+    "version": "1.0.0"
+  },
+  "download": {
+    "ios": "https://itunes.apple.com/cn/app/hello-uni-app/id1417078253?mt=8",
+    "android": "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-97fca9f2-41f6-449f-a35e-3f135d4c3875/6d754387-a6c3-48ed-8ad2-e8f39b40fc01.apk"
+  },
+  "marketId": {
+    "ios": "",
+    "android": ""
+  },
+  "i18n": {
+    "enable": true
+  }
+};
+exports.config = config;
